8b8bd1e67fabc4f3e0417118cdd268f4d8f2b1f9
Sachin Gupta <sgs.engineer@gmail.com>
2018-04-01 17:48:24 +0530

    Google Site Search Changes

---
+++ search-results.php
@@ -0,0 +1,12 @@
+<script>
+    (function() {
+        var cx = '011180747518809242029:v32bfft31ci';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+    })();
+</script>
+<gcse:search></gcse:search>
